39bb2bc34f97ce644eac15df6391471a117a27cc
Ayush Verma <vermaayush.verma76@gmail.com>
2018-12-17 23:16:00 +0530

    added flatButtons and multiscreens.

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ToDo app',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new FirstScreen(),
@@ -33,11 +24,13 @@ class FirstScreen extends StatelessWidget{
         title: new Text("Google Sign In"),
       ),
       body: Center(
-        child: RaisedButton(
+        child: OutlineButton(
           child: Text("Sign In"),
           onPressed: (){
             Navigator.push(context, 
             MaterialPageRoute(builder: (context) => SecondScreen())
+            shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(80.0));
+            BorderSide: BorderSide(color: Colors.blue);
             );
           },
         ),
@@ -53,9 +46,11 @@ class SecondScreen extends StatelessWidget {
         title: Text("To Do App"),
       ),
       body: Center(
-        child: FloatingActionButton(
+        child: OutlineButton(
           onPressed: () {
             Navigator.pop(context);
+            shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(80.0));
+            BorderSide: BorderSide(color: Colors.blue);
           },
           child: Text('Logout'),
         ),
