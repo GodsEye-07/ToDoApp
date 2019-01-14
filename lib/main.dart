feb973891ab14eb6d82e1bd7dfb4250cf38d554d
Ayush Verma <vermaayush.verma76@gmail.com>
2019-01-14 11:42:08 +0530

    created the basic app and removed the firebase auth.

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,131 @@
+// Import MaterialApp and other widgets which we can use to quickly create a material app
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
-void main() => runApp(MyApp());
+// Code written in Dart starts exectuting from the main function. runApp is part of
+// Flutter, and requires the component which will be our app's container. In Flutter,
+// every component is known as a "widget".
+void main() => runApp(new TodoApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+// Every component in Flutter is a widget, even the whole app itself
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ToDo app',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new FirstScreen(),
+    return new MaterialApp(
+      title: 'Todo List',
+      home: new TodoList()
     );
   }
 }
 
-class FirstScreen extends StatelessWidget{
+class TodoList extends StatefulWidget {
+  @override
+  createState() => new TodoListState();
+}
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn googleSignIn = new GoogleSignIn();
+class TodoListState extends State<TodoList> {
+  List<String> _todoItems = [];
 
-  Future<FirebaseUser> _SignIn() async{
+  void _addTodoItem(String task) {
+    // Only add the task if the user actually entered something
+    if(task.length > 0) {
+      // Putting our code inside "setState" tells the app that our state has changed, and
+      // it will automatically re-render the list
+      setState(() => _todoItems.add(task));
+    }
+  }
 
-    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-    GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
 
-    FirebaseUser user = await _auth.signInWithGoogle(
-       idToken: gSA.idToken, accessToken: gSA.accessToken
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text('Mark "${_todoItems[index]}" as done?'),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('CANCEL'),
+              // The alert is actually part of the navigation stack, so to close it, we
+              // need to pop it.
+              onPressed: () => Navigator.of(context).pop()
+            ),
+            new FlatButton(
+              child: new Text('MARK AS DONE'),
+              onPressed: () {
+                _removeTodoItem(index);
+                Navigator.of(context).pop();
+              }
+            )
+          ]
+        );
+      }
     );
+  }
 
-    print('username : ${user.displayName}');
+  // Build the whole list of todo items
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        // itemBuilder will be automatically be called as many times as it takes for the
+        // list to fill up its available space, which is most likely more than the
+        // number of todo items we have. So, we need to check the index is OK.
+        if(index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index], index);
+        }
+      },
+    );
+  }
 
-    return user;
-
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText, int index) {
+    return new ListTile(
+      title: new Text(todoText),
+      onTap: () => _promptRemoveTodoItem(index)
+    );
   }
 
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Google Sign In"),
+        title: new Text('Todo List')
       ),
-      body: Center(
-        child: OutlineButton(
-          child: Text("Sign In"),
-          onPressed: (){
-            Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => SecondScreen());
-            shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(80.0));
-            BorderSide: BorderSide(color: Colors.blue);
-            );
-          },
-        ),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _pushAddTodoScreen,
+        tooltip: 'Add task',
+        child: new Icon(Icons.add)
       ),
     );
   }
-}
-class SecondScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("To Do App"),
-      ),
-      body: new Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: new Column(
-          children: <Widget>[
-            new RaisedButton(
-              onPressed: () => _signIn().then(),
-              child: new Text("Sign In"),
-              color: Colors.green,
-            ),
-            new Padding(
-              padding: const EdgeInsets.all(10.0),
-            ),
-            new RaisedButton(
-              onPressed: null,
-              child: new Text("Sign Out"),
-              color: Colors.red,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
 
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+      // MaterialPageRoute will automatically animate the screen entry, as well as adding
+      // a back button to close it
+      new MaterialPageRoute(
+        builder: (context) {
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Add a new task')
+            ),
+            body: new TextField(
+              autofocus: true,
+              onSubmitted: (val) {
+                _addTodoItem(val);
+                Navigator.pop(context); // Close the add todo screen
+              },
+              decoration: new InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: const EdgeInsets.all(16.0)
+              ),
+            )
+          );
+        }
+      )
+    );
+  }
+}
